e8da4e151896bb987d956df33dc84f79609573ab
unknown <amineneymar10@gmail.com>
2024-03-03 21:22:31 +0100

    new

--- ChaTay/registration/views.py
+++ ChaTay/registration/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def register(request):
-    return HttpResponse("Hello, world. You're")
+    return render(request, "registrationStyle/index.html")
